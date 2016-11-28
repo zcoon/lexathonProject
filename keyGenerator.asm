e3dfccad1acd81e86ebc7479f2931970dbb4f93c
Zachary Cooner <zacharycooner@gmail.com>
2016-11-27 20:48:54 -0600

    cleaned up keyGenerator

--- keyGenerator.asm
+++ keyGenerator.asm
@@ -23,9 +23,9 @@ vowelU: .byte 'u'
 .text
 
 main:
-	li $v0, 4
-	la $a0, startingKeyGen #Prints "Starting keyGen"
-	syscall
+	#li $v0, 4
+	#la $a0, startingKeyGen #Prints "Starting keyGen"
+	#syscall
 	
 	li   $v0, 41       	# get a random int
 	syscall
@@ -35,13 +35,13 @@ main:
 	addi $v0, $v0, 1	# add 1 to modValue to handle case where the div operation results in 0
 	move $s0, $v0 		# move the mod value to $s0
 	
-	li $v0, 4
-	la $a0, modValueIs 	# Prints statement "The mod value is "
-	syscall
+	#li $v0, 4
+	#la $a0, modValueIs 	# Prints statement "The mod value is "
+	#syscall
 	
 	move $a0, $s0 		# move the modValue to $a0
-	li $v0, 1 		# print the mod value, this should be 0, 1, 2, 3 or 4
-	syscall
+	#li $v0, 1 		# print the mod value, this should be 0, 1, 2, 3 or 4
+	#syscall
 	
 	jal getVowel 		# Call the function to return 1 vowel
 	
@@ -54,16 +54,16 @@ main:
 	syscall
 	
 	li $v0, 4
-	la $a0, goodbye # Terminate the program
+	la $a0, goodbye # Print "Terminating"
 	syscall
 	
-	li $v0, 10
+	li $v0, 10 # Terminate program
 	syscall
 	
 modValueOne:
-	li $v0, 4
-	la $a0, inModValueOne
-	syscall
+	#li $v0, 4
+	#la $a0, inModValueOne #Log Statement
+	#syscall
 	
 	la $a0, vowelA  # Get the address
 	lb $v1, ($a0)  # Get the value at that address
@@ -71,9 +71,9 @@ modValueOne:
 	jr $ra
 	
 modValueTwo:
-	li $v0, 4
-	la $a0, inModValueTwo
-	syscall
+	#li $v0, 4
+	#la $a0, inModValueTwo #Log Statement
+	#syscall
 	
 	la $a0, vowelE  # Get the address
 	lb $v1, ($a0)  # Get the value at that address
@@ -81,9 +81,9 @@ modValueTwo:
 	jr $ra
 	
 modValueThree:
-	li $v0, 4
-	la $a0, inModValueThree
-	syscall
+	#li $v0, 4
+	#la $a0, inModValueThree #Log Statement
+	#syscall
 	
 	la $a0, vowelI  # Get the address
 	lb $v1, ($a0)  # Get the value at that address
@@ -91,9 +91,9 @@ modValueThree:
 	jr $ra
 	
 modValueFour:
-	li $v0, 4
-	la $a0, inModValueFour
-	syscall
+	#li $v0, 4
+	#la $a0, inModValueFour #Log Statement
+	#syscall
 	
 	la $a0, vowelO  # Get the address
 	lb $v1, 0($a0)  # Get the value at that address
@@ -101,9 +101,9 @@ modValueFour:
 	jr $ra
 	
 modValueFive:
-	li $v0, 4
-	la $a0, inModValueFive
-	syscall
+	#li $v0, 4
+	#la $a0, inModValueFive #Log Statement
+	#syscall
 	
 	la $a0, vowelU  # Get the address
 	lb $v1, ($a0)  # Get the value at that address
@@ -112,20 +112,11 @@ modValueFive:
 
 getVowel:
 	#a0 holds the mod value, which is 1, 2, 3, 4 or 5 to be mapped to a variable
-	move $a1, $a0 #Copy value of $a0 into $a1
 
-	beq $a1, 1 modValueOne #branch if $s0 == 1
-	beq $a1, 2 modValueTwo #branch if $s0 == 2
-	beq $a1, 3 modValueThree #branch if $s0 == 3
-	beq $a1, 4 modValueFour #branch if $s0 == 4
-	beq $a1, 5 modValueFive #branch if $s0 == 5
+	beq $a0, 1 modValueOne #branch if $s0 == 1
+	beq $a0, 2 modValueTwo #branch if $s0 == 2
+	beq $a0, 3 modValueThree #branch if $s0 == 3
+	beq $a0, 4 modValueFour #branch if $s0 == 4
+	beq $a0, 5 modValueFive #branch if $s0 == 5
 	
-	#The following is a hard coded solution to always return vowelA
-	#la $a0, vowelA  # Get the address of the vowel
-	#lb $v1, 0($a0)  # Store the contents of the vowelAddress into $v1 to return
-	
-	jr $ra
-	
-fixRemainder:
-	move $v0, $zero
 	jr $ra
