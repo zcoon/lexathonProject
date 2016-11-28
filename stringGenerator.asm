90a1b032af95712f09ee4c9b97d92cff9c7661ee
Zachary Cooner <zacharycooner@gmail.com>
2016-11-27 23:25:00 -0600

    modValue functions populated

--- stringGenerator.asm
+++ stringGenerator.asm
@@ -73,7 +73,7 @@ loop:
 	
 	divu $t2, $a0, 26  	# mod the value in $a0 by 26 and store in $t2 so we can generate a letter number
 	mfhi $v0		# Get the remainder value
-	addi $v0, $v0, 1	# add 1 to modValue to handle case where the div operation results in 0
+	addi $v0, $v0, 1	# add 1 to modValue to make numbers match those of the alphabet
 	move $s0, $v0 		# move the mod value to $s0 to use elsewhere
 	
 	li $v0, 4
@@ -93,34 +93,294 @@ exit:
 	jr $ra # return us to the caller, which brings us back to main
 	
 getLetter:
-	#a0 holds the mod value, which is 1, 2, 3, 4 or 5 to be mapped to a variable
+	#a0 holds the mod value, which should be 1 through 26 to be mapped to a letter of the alphabet
 
 	beq $a0, 1 modValueOne #branch if $s0 == 1
 	beq $a0, 2 modValueTwo #branch if $s0 == 2
 	beq $a0, 3 modValueThree #branch if $s0 == 3
 	beq $a0, 4 modValueFour #branch if $s0 == 4
 	beq $a0, 5 modValueFive #branch if $s0 == 5
-	beq $a0, 6 modValueSix #branch if $s0 == 1
-	beq $a0, 7 modValueSeven #branch if $s0 == 2
-	beq $a0, 8 modValueEight #branch if $s0 == 3
-	beq $a0, 9 modValueNine #branch if $s0 == 4
-	beq $a0, 10 modValueTen #branch if $s0 == 5
-	beq $a0, 11 modValueEleven #branch if $s0 == 1
-	beq $a0, 12 modValueTwelve #branch if $s0 == 2
-	beq $a0, 13 modValueThirteen #branch if $s0 == 3
-	beq $a0, 14 modValueFourteen #branch if $s0 == 4
-	beq $a0, 15 modValueFifteen #branch if $s0 == 5
-	beq $a0, 16 modValueSixteen #branch if $s0 == 1
-	beq $a0, 17 modValueSeventeen #branch if $s0 == 2
-	beq $a0, 18 modValueEighteen #branch if $s0 == 3
-	beq $a0, 19 modValueNineteen #branch if $s0 == 4
-	beq $a0, 20 modValueTwenty #branch if $s0 == 5
-	beq $a0, 21 modValueTwentyOne #branch if $s0 == 1
-	beq $a0, 22 modValueTwentyTwo #branch if $s0 == 2
-	beq $a0, 23 modValueTwentyThree #branch if $s0 == 3
-	beq $a0, 24 modValueTwentyFour #branch if $s0 == 4
-	beq $a0, 25 modValueTwentyFive #branch if $s0 == 5
-	beq $a0, 26 modValueTwentySix #branch if $s0 == 5
+	beq $a0, 6 modValueSix #branch if $s0 == 6
+	beq $a0, 7 modValueSeven #branch if $s0 == 7
+	beq $a0, 8 modValueEight #branch if $s0 == 8
+	beq $a0, 9 modValueNine #branch if $s0 == 9
+	beq $a0, 10 modValueTen #branch if $s0 == 10
+	beq $a0, 11 modValueEleven #branch if $s0 == 11
+	beq $a0, 12 modValueTwelve #branch if $s0 == 12
+	beq $a0, 13 modValueThirteen #branch if $s0 == 13
+	beq $a0, 14 modValueFourteen #branch if $s0 == 14
+	beq $a0, 15 modValueFifteen #branch if $s0 == 15
+	beq $a0, 16 modValueSixteen #branch if $s0 == 16
+	beq $a0, 17 modValueSeventeen #branch if $s0 == 17
+	beq $a0, 18 modValueEighteen #branch if $s0 == 18
+	beq $a0, 19 modValueNineteen #branch if $s0 == 19
+	beq $a0, 20 modValueTwenty #branch if $s0 == 20
+	beq $a0, 21 modValueTwentyOne #branch if $s0 == 21
+	beq $a0, 22 modValueTwentyTwo #branch if $s0 == 22
+	beq $a0, 23 modValueTwentyThree #branch if $s0 == 23
+	beq $a0, 24 modValueTwentyFour #branch if $s0 == 24
+	beq $a0, 25 modValueTwentyFive #branch if $s0 == 25
+	beq $a0, 26 modValueTwentySix #branch if $s0 == 26
 	
 	jr $ra
 
+modValueOne:
+	#li $v0, 4
+	#la $a0, inModValueOne #Log Statement
+	#syscall
+	
+	la $a0, letterA  # Get the address
+	lb $v1, ($a0)  # Get the value at that address
+	
+	jr $ra
+	
+modValueTwo:
+	#li $v0, 4
+	#la $a0, inModValueOne #Log Statement
+	#syscall
+	
+	la $a0, letterB  # Get the address
+	lb $v1, ($a0)  # Get the value at that address
+	
+	jr $ra
+	
+modValueThree:
+	#li $v0, 4
+	#la $a0, inModValueOne #Log Statement
+	#syscall
+	
+	la $a0, letterC  # Get the address
+	lb $v1, ($a0)  # Get the value at that address
+	
+	jr $ra
+	
+modValueFour:
+	#li $v0, 4
+	#la $a0, inModValueOne #Log Statement
+	#syscall
+	
+	la $a0, letterD  # Get the address
+	lb $v1, ($a0)  # Get the value at that address
+	
+	jr $ra
+	
+modValueFive:
+	#li $v0, 4
+	#la $a0, inModValueOne #Log Statement
+	#syscall
+	
+	la $a0, letterE  # Get the address
+	lb $v1, ($a0)  # Get the value at that address
+	
+	jr $ra
+	
+modValueSix:
+	#li $v0, 4
+	#la $a0, inModValueOne #Log Statement
+	#syscall
+	
+	la $a0, letterF  # Get the address
+	lb $v1, ($a0)  # Get the value at that address
+	
+	jr $ra
+	
+modValueSeven:
+	#li $v0, 4
+	#la $a0, inModValueOne #Log Statement
+	#syscall
+	
+	la $a0, letterG  # Get the address
+	lb $v1, ($a0)  # Get the value at that address
+	
+	jr $ra
+	
+modValueEight:
+	#li $v0, 4
+	#la $a0, inModValueOne #Log Statement
+	#syscall
+	
+	la $a0, letterH  # Get the address
+	lb $v1, ($a0)  # Get the value at that address
+	
+	jr $ra
+	
+modValueNine:
+	#li $v0, 4
+	#la $a0, inModValueOne #Log Statement
+	#syscall
+	
+	la $a0, letterI  # Get the address
+	lb $v1, ($a0)  # Get the value at that address
+	
+	jr $ra
+	
+modValueTen:
+	#li $v0, 4
+	#la $a0, inModValueOne #Log Statement
+	#syscall
+	
+	la $a0, letterJ  # Get the address
+	lb $v1, ($a0)  # Get the value at that address
+	
+	jr $ra
+	
+modValueEleven:
+	#li $v0, 4
+	#la $a0, inModValueOne #Log Statement
+	#syscall
+	
+	la $a0, letterK  # Get the address
+	lb $v1, ($a0)  # Get the value at that address
+	
+	jr $ra
+	
+modValueTwelve:
+	#li $v0, 4
+	#la $a0, inModValueOne #Log Statement
+	#syscall
+	
+	la $a0, letterL  # Get the address
+	lb $v1, ($a0)  # Get the value at that address
+	
+	jr $ra
+	
+modValueThirteen:
+	#li $v0, 4
+	#la $a0, inModValueOne #Log Statement
+	#syscall
+	
+	la $a0, letterM  # Get the address
+	lb $v1, ($a0)  # Get the value at that address
+	
+	jr $ra
+	
+modValueFourteen:
+	#li $v0, 4
+	#la $a0, inModValueOne #Log Statement
+	#syscall
+	
+	la $a0, letterN  # Get the address
+	lb $v1, ($a0)  # Get the value at that address
+	
+	jr $ra
+	
+modValueFifteen:
+	#li $v0, 4
+	#la $a0, inModValueOne #Log Statement
+	#syscall
+	
+	la $a0, letterO  # Get the address
+	lb $v1, ($a0)  # Get the value at that address
+	
+	jr $ra
+	
+modValueSixteen:
+	#li $v0, 4
+	#la $a0, inModValueOne #Log Statement
+	#syscall
+	
+	la $a0, letterP  # Get the address
+	lb $v1, ($a0)  # Get the value at that address
+	
+	jr $ra
+	
+modValueSeventeen:
+	#li $v0, 4
+	#la $a0, inModValueOne #Log Statement
+	#syscall
+	
+	la $a0, letterQ  # Get the address
+	lb $v1, ($a0)  # Get the value at that address
+	
+	jr $ra
+	
+modValueEighteen:
+	#li $v0, 4
+	#la $a0, inModValueOne #Log Statement
+	#syscall
+	
+	la $a0, letterR  # Get the address
+	lb $v1, ($a0)  # Get the value at that address
+	
+	jr $ra
+	
+modValueNineteen:
+	#li $v0, 4
+	#la $a0, inModValueOne #Log Statement
+	#syscall
+	
+	la $a0, letterS  # Get the address
+	lb $v1, ($a0)  # Get the value at that address
+	
+	jr $ra
+	
+modValueTwenty:
+	#li $v0, 4
+	#la $a0, inModValueOne #Log Statement
+	#syscall
+	
+	la $a0, letterT  # Get the address
+	lb $v1, ($a0)  # Get the value at that address
+	
+	jr $ra
+	
+modValueTwentyOne:
+	#li $v0, 4
+	#la $a0, inModValueOne #Log Statement
+	#syscall
+	
+	la $a0, letterU  # Get the address
+	lb $v1, ($a0)  # Get the value at that address
+	
+	jr $ra
+	
+modValueTwentyTwo:
+	#li $v0, 4
+	#la $a0, inModValueOne #Log Statement
+	#syscall
+	
+	la $a0, letterV  # Get the address
+	lb $v1, ($a0)  # Get the value at that address
+	
+	jr $ra
+	
+modValueTwentyThree:
+	#li $v0, 4
+	#la $a0, inModValueOne #Log Statement
+	#syscall
+	
+	la $a0, letterW  # Get the address
+	lb $v1, ($a0)  # Get the value at that address
+	
+	jr $ra
+	
+modValueTwentyFour:
+	#li $v0, 4
+	#la $a0, inModValueOne #Log Statement
+	#syscall
+	
+	la $a0, letterX  # Get the address
+	lb $v1, ($a0)  # Get the value at that address
+	
+	jr $ra
+	
+modValueTwentyFive:
+	#li $v0, 4
+	#la $a0, inModValueOne #Log Statement
+	#syscall
+	
+	la $a0, letterY  # Get the address
+	lb $v1, ($a0)  # Get the value at that address
+	
+	jr $ra
+	
+		
+modValueTwentySix:
+	#li $v0, 4
+	#la $a0, inModValueOne #Log Statement
+	#syscall
+	
+	la $a0, letterZ  # Get the address
+	lb $v1, ($a0)  # Get the value at that address
+	
+	jr $ra
