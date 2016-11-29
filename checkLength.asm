6c1c99bc43b802703bee209698e24a0c70b5536d
drana1022 <drana1022@gmail.com>
2016-11-29 16:42:36 -0600

    Update checkLength.asm

--- checkLength.asm
+++ checkLength.asm
@@ -1,34 +1,14 @@
 
 
  .data
- userInput: .space 9	
- str1:  .asciiz "Enter a word (4-9 letters): "
 string: .asciiz "test string"
 printLength: .asciiz "The length of the string: "
  .text
  
  
 main:
-	la $a0,str1 #Prompting the user for input
-         li $v0,4
-         syscall
-         
-          li $v0,8 #take in input
-         la $a0, userInput #load byte space into address
-         li $a1, 9 # allot the byte space for string
-         move $t0,$a0 #save string to t0
-         syscall
-         
-         la $a0, userInput #reload byte space to primary address
-         move $a0,$t0 # primary address = t0 address (load pointer)
-         syscall
-         
-         
-
-
-
- 	la $a0, ($t0)           # Load address of string.
-         jal checkLength              # Call checkLength procedure.
+  la $a0, string             # Load address of string.
+        jal checkLength              # Call checkLength procedure.
         jal print
      	addi $a1, $a0, 0        # Move address of string to $a1
         addi $v1, $v0, 0        # Move length of string to $v1
@@ -63,13 +43,3 @@ main:
   	  syscall
 
 	   jr $ra
-#************************Printing the length of the string*************************************
-#NOTE: This function is only for testing. 
-#Need to return boolean 0 for invalid input
-#Need to return 1 for valid input (4-9) characters.
-
-
-
-
-
-
