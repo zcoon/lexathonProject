8ed76888c6bfc5788def0fed65a9c3fb16edc84c
jugar96 <juan_garcia_96@yahoo.ca>
2016-11-30 19:07:54 -0600

    Update LexathonMain.asm

--- LexathonMain.asm
+++ LexathonMain.asm
@@ -52,6 +52,8 @@
 	letterX: .byte 'x'
 	letterY: .byte 'y'
 	letterZ: .byte 'z'
+	
+	duplicateFile: .asciiz "/Users/juangarcia/duplicateFile.txt"
 .text
 startScreen:	
 
@@ -256,7 +258,7 @@ printGameGridHalf:
      	##################### jump to the check file, to determine if the word is valid
      	
      	##################### if the word is valid
-     	##################### -increment timer
+     	jal writeWordToDuplicateFile
      	##################### -write word to duplicate file
      	##################### -increment score
 	##################### jump back up to the userInput function
@@ -507,3 +509,50 @@ modValueTwentySix:
 	la $a0, letterZ  # Get the address
 	lb $v1, ($a0)  # Get the value at that address
 	jr $ra
+
+writeWordToDuplicateFile:
+	lb $t1, nullTerminator
+do:    	lb $t2, myword($t0)
+	beq $t2, $t1, endWhile     #calculates the length of myword and stores it in $t0	
+    	addi $t0, $t0, 1
+    	j do
+endWhile:		
+
+	li $v0, 13
+    	la $a0, duplicateFile	#open file for write/append
+   	li $a1, 9
+   	li $a2, 0
+   	syscall  # File descriptor gets returned in $v0
+
+    	move $a0, $v0  # Syscall 15 requires file descriptor in $a0
+    	li $v0, 15
+    	la $a1, myword
+    	
+    	bne $t0, 4, check5
+    	li $a2, 4  
+    	j doneCheck
+check5: bne $t0, 5, check6
+    	li $a2, 5  
+    	j doneCheck
+check6: bne $t0, 6, check7	#These checks are necessary becuase writing to the file 
+    	li $a2, 6  		#requires the hardcoded length of the word to be written
+    	j doneCheck		#so we need to hardcode all 6 options
+check7: bne $t0, 7, check8
+    	li $a2, 7  
+    	j doneCheck
+check8: bne $t0, 8, check9
+    	li $a2, 8  
+    	j doneCheck
+check9: 
+    	li $a2, 9  
+    	
+doneCheck:
+    	syscall			#word is appened to the file
+    	
+    	li $v0, 15
+    	la $a1, newline		#with a newline after it
+    	li $a2, 1  
+    	syscall
+
+    	li $v0, 16  # $a0 already has the file descriptor. Closes the file 	
+endWriteWordToDuplicateFile:
